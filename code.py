4f3506a11f08ed0a070f0b76c45c9fd1f465e03d
alainng <alainng@users.noreply.github.com>
2018-01-13 09:24:23 -0500

    added natural repeat

--- code.py
+++ code.py
@@ -13,8 +13,6 @@ import sys
 x_pad = 38
 y_pad = 29
 
-#Prepare battle 1678 910
-#Get Ready For battle 1402 929
 
 def screenGrab():
     box = (x_pad+1,y_pad+1,x_pad+1808,y_pad+1018)
@@ -62,7 +60,13 @@ def leftClick():
 def mousePos(cord):
     win32api.SetCursorPos((x_pad + cord[0], y_pad + cord[1]))
 
-def get_cords():
+def moveClickSleep(cord,seconds):
+    win32api.SetCursorPos((x_pad + cord[0], y_pad + cord[1]))
+    leftClick()
+    time.sleep(seconds)
+
+    
+def get_cords(): #debug function
     x,y = win32api.GetCursorPos()
     x = x - x_pad
     y = y - y_pad
@@ -75,18 +79,11 @@ def start_repeat_fight():
         if(grab_mail_status()!=23543):
             collect_mail()
         #start fight sequence
-        mousePos(Cord.prepare_battle)
-        leftClick()
-        time.sleep(.75)
-        mousePos(Cord.get_ready_for_battle)
-        leftClick()
-        time.sleep(.75)
-        mousePos(Cord.auto_repeat)
-        leftClick()
-        time.sleep(.75)
-        mousePos(Cord.auto_repeat_ok)
-        leftClick()
-        time.sleep(5)
+        moveClickSleep(Cord.prepare_battle,0.75)
+        moveClickSleep(Cord.get_ready_for_battle,0.75)
+        moveClickSleep(Cord.auto_repeat,0.75)
+        moveClickSleep(Cord.auto_repeat_ok,5)
+
         #in fight
         stamina_check_counter=0
         while (grab_insufficient_stamina()!=100182 and grab_insufficient_stamina()!=99937):
@@ -94,60 +91,61 @@ def start_repeat_fight():
                 sys.exit()
             time.sleep(5)
             stamina_check_counter = stamina_check_counter +1
-        mousePos(Cord.insufficient_stamina_ok)
-        leftClick()
-        time.sleep(2)
-        mousePos(Cord.middle_stamina_potion) #make sure that stamina recovery items > 1 run
-        leftClick()
-        time.sleep(2)
-        mousePos(Cord.middle_stamina_potion_ok)
-        leftClick()
-        time.sleep(1)
-        mousePos(Cord.middle_stamina_potion_ok_yes)
-        leftClick()
-        time.sleep(1)
-        mousePos(Cord.middle_stamina_potion_ok_yes_x)
-        leftClick()
-        time.sleep(1)
+        
+        moveClickSleep(Cord.insufficient_stamina_ok,2)
+        #make sure that stamina recovery items > 1 run
+        moveClickSleep(Cord.middle_stamina_potion,2)
+        moveClickSleep(Cord.middle_stamina_potion_ok,1)
+        moveClickSleep(Cord.middle_stamina_potion_ok_yes,3)
+        moveClickSleep(Cord.middle_stamina_potion_ok_yes_x,1)
+        
         sell_all_post_fight()
         repeat_counter=repeat_counter-1
     print(repeat_counter)
-    
-  
+
+def start_natural_fight():
+    repeat_counter=150
+    stamina_to_gain=36
+    time_per_stamina=49
+    time_to_wait=stamina_to_gain*time_per_stamina
+    while(repeat_counter>0):
+        if(grab_mail_status()!=23543):
+            collect_mail()
+        #start fight sequence
+        moveClickSleep(Cord.prepare_battle,0.75)
+        moveClickSleep(Cord.get_ready_for_battle,0.75)
+        moveClickSleep(Cord.auto_repeat,0.75)
+        moveClickSleep(Cord.auto_repeat_ok,5)
+        
+        #in fight
+        stamina_check_counter=0
+        while (grab_insufficient_stamina()!=100182 and grab_insufficient_stamina()!=99937):
+            if(stamina_check_counter > 360):
+                sys.exit()
+            time.sleep(5)
+            stamina_check_counter = stamina_check_counter +1
+        moveClickSleep(Cord.insufficient_stamina_ok,2)
+        moveClickSleep(Cord.middle_stamina_potion_x,1)
+        moveClickSleep(Cord.post_fight_exit,1)
+        time.sleep(time_to_wait)
+        
+        repeat_counter=repeat_counter-1
+    print(repeat_counter)
     
 def sell_all_post_fight():
-    mousePos(Cord.post_fight_inventory)
-    leftClick()
-    time.sleep(15)
-    mousePos(Cord.i_sell)
-    leftClick()
-    time.sleep(.75)
-    mousePos(Cord.i_sellgrind_all)
-    leftClick()
-    time.sleep(.75)
-    mousePos(Cord.i_sellgrind_all_confirmation)
-    leftClick()
-    time.sleep(.75)
-    mousePos(Cord.i_sellgrind_all_confirmation_confirm)
-    leftClick()
-    time.sleep(1.25)
-    mousePos(Cord.back)
-    leftClick()
-    time.sleep(3)
+    moveClickSleep(Cord.post_fight_inventory,15)
+    moveClickSleep(Cord.i_sell,1)
+    moveClickSleep(Cord.i_sellgrind_all,1)
+    moveClickSleep(Cord.i_sellgrind_all_confirmation,1)
+    moveClickSleep(Cord.i_sellgrind_all_confirmation_confirm,1.25)
+    moveClickSleep(Cord.back,3)
+   
 
-def collect_mail(): #untested
-    mousePos(Cord.mail)
-    leftClick()
-    time.sleep(2)
-    mousePos(Cord.m_claim_all)
-    leftClick()
-    time.sleep(3)
-    mousePos(Cord.m_claim_all)
-    leftClick()
-    time.sleep(1)
-    mousePos(Cord.m_x)
-    leftClick()
-    time.sleep(1)
+def collect_mail():
+    moveClickSleep(Cord.mail,3)
+    moveClickSleep(Cord.m_claim_all,4)
+    moveClickSleep(Cord.m_claim_all,2)
+    moveClickSleep(Cord.m_x,1)
     
 
     
